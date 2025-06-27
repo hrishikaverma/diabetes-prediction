31e20e2b633e659670ccca6188f5b860e12648f3
Hrishika verma <hrishikaverma2@gmail.com>
2025-06-27 18:51:00 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -128,7 +128,7 @@ if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    set_bg("bg.jpg")
+    set_bg("Bg.jpg")
     login_page()
     st.stop()
 else:
