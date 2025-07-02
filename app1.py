9299bf4eba2081d4e3341a0e120ce4cbbd8a5889
Hrishika verma <hrishikaverma2@gmail.com>
2025-07-02 17:45:31 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -74,4 +74,4 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=10000, debug=True, use_reloader=False)
